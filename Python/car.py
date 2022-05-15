db49f6d6589324877cb38e557a1d38e10589b659
alebeta <alebeta0000@gmail.com>
2022-05-14 22:30:03 -0500

    Practica de POO

---
+++ Python/car.py
@@ -0,0 +1,11 @@
+from account import Account
+
+class Car:
+    id = int
+    license = str
+    driver = Account("","")
+    passegenger = int
+
+    def __init__(self, license, driver):
+        self.license    = license
+        self.driver     = driver
